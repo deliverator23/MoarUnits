2229ee2466d1b6aaff7287436a45894f59db558d
iainmillar23 <iain.millar@gmail.com>
2016-11-17 16:21:55 +0000

    Commit initial mod files

---
+++ MOAR_Units/MOAR_Units.sql
@@ -0,0 +1,101 @@
+UPDATE UnitUpgrades
+SET    UpgradeUnit = 'UNIT_RIFLEMAN'
+WHERE  Unit = 'UNIT_MUSKETMAN';
+
+UPDATE UnitUpgrades
+SET    UpgradeUnit = 'UNIT_CUIRASSIER'
+WHERE  Unit = 'UNIT_KNIGHT';
+
+/*
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_SOHEI';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_AMAZON';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_FATHERLAND_VOLUNTEER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_BANDEIRANTE';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_HYKSOS_BOWMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_GENDARME';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_GARDE_REPUBLICAINE';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_KNIGHT_UNMOUNTED';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1'
+WHERE  UnitType = 'UNIT_CHOKONU';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_JINETE';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_DRUZHINA';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_HIRDMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_ENGLISH_REDCOAT';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_MINUTEMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_CROSSBOWMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_ARCHER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_BUILDER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_WARRIOR';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_COMPANION_CAVALRY';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_JINETE';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_EQUITE';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_CUIRASSIER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, TraitType=NULL,  Cost='1', BaseMoves='100'
+WHERE  UnitType = 'UNIT_LONGBOWMAN';
+ */
