630233e645220f9be888bac381044ea48e733562
iainmillar23 <shaihulud1>
2017-01-23 14:31:35 +0000

    0.7 final changes.

--- MOAR_Units/Data/MOAR_Units_Data.sql
+++ MOAR_Units/Data/MOAR_Units_Data.sql
@@ -8,6 +8,10 @@ UPDATE UnitUpgrades SET UpgradeUnit = 'UNIT_CUIRASSIER' WHERE Unit = 'UNIT_INDIA
 
 UPDATE UnitUpgrades SET UpgradeUnit = 'UNIT_MACEMAN' WHERE Unit = 'UNIT_SWORDSMAN';
 
+UPDATE UnitUpgrades SET UpgradeUnit = 'UNIT_MACEMAN' WHERE Unit = 'UNIT_ROMAN_LEGION';
+
+UPDATE UnitUpgrades SET UpgradeUnit = 'UNIT_MACEMAN' WHERE Unit = 'UNIT_KONGO_SHIELD_BEARER';
+
 UPDATE ModifierArguments SET Value = 9 WHERE ModifierId = 'CONQUISTADOR_SPECIFIC_UNIT_COMBAT' AND Name = 'Amount';
 
 /* Expanded Recon Class */
@@ -22,26 +26,34 @@ UPDATE Units SET Range = 2 WHERE UnitType = 'UNIT_MACHINE_GUN';
 UPDATE Units SET BaseSightRange = 2 WHERE UnitType='UNIT_NATURALIST';
 
 /* Expanded Recon Promotions Rework */
-UPDATE UnitPromotions SET Level = 2, Column = 3 WHERE UnitPromotionType='PROMOTION_GUERRILLA';
-UPDATE UnitPromotions SET Level = 3, Column = 1 WHERE UnitPromotionType='PROMOTION_SPYGLASS';
+UPDATE UnitPromotions SET Name='LOC_PROMOTION_SPYGLASS_NAME', Description='LOC_PROMOTION_SPYGLASS_DESCRIPTION' WHERE UnitPromotionType='PROMOTION_GUERRILLA';
+UPDATE UnitPromotions SET Name='LOC_PROMOTION_GUERRILLA_NAME', Description='LOC_PROMOTION_GUERRILLA_DESCRIPTION' WHERE UnitPromotionType='PROMOTION_SPYGLASS';
 
-DELETE FROM UnitPromotionPrereqs WHERE UnitPromotion = 'PROMOTION_GUERRILLA';
-DELETE FROM UnitPromotionPrereqs WHERE UnitPromotion = 'PROMOTION_SPYGLASS';
-DELETE FROM UnitPromotionPrereqs WHERE UnitPromotion = 'PROMOTION_AMBUSH';
-DELETE FROM UnitPromotionPrereqs WHERE UnitPromotion = 'PROMOTION_CAMOUFLAGE';
-
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_SPYGLASS','PROMOTION_RANGER');
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_SPYGLASS','PROMOTION_ALPINE');
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_GUERRILLA','PROMOTION_SENTRY');
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_AMBUSH','PROMOTION_SPYGLASS');
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_CAMOUFLAGE','PROMOTION_GUERRILLA');
-INSERT INTO UnitPromotionPrereqs (UnitPromotion, PrereqUnitPromotion) VALUES ('PROMOTION_CAMOUFLAGE','PROMOTION_AMBUSH');
+UPDATE UnitPromotionModifiers SET ModifierId = 'FADE_BONUS_MOVE_AFTER_ATTACKING' WHERE UnitPromotionType='PROMOTION_SPYGLASS';
+UPDATE UnitPromotionModifiers SET ModifierId = 'SPYGLASS_BONUS_SIGHT' WHERE UnitPromotionType='PROMOTION_GUERRILLA';
 
 INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES ('AMBUSH_REQUIREMENTS','REQUIREMENTSET_TEST_ALL');
 INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES ('AMBUSH_REQUIREMENTS','PLAYER_IS_DEFENDER_REQUIREMENTS');
-UPDATE Modifier SET SubjectRequirementSetId = 'AMBUSH_REQUIREMENTS' WHERE ModifierId = 'AMBUSH_INCREASED_COMBAT_STRENGTH';
+UPDATE Modifiers SET SubjectRequirementSetId = 'AMBUSH_REQUIREMENTS' WHERE ModifierId = 'AMBUSH_INCREASED_COMBAT_STRENGTH';
 
-/*
+
+/* Commented Out!
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100', MustPurchase=0
+WHERE  UnitType = 'UNIT_EXPLORER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100', MustPurchase=0
+WHERE  UnitType = 'UNIT_SNIPER';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100', MustPurchase=0
+WHERE  UnitType = 'UNIT_MACEMAN';
+
+UPDATE Units
+SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100', MustPurchase=0
+WHERE  UnitType = 'UNIT_MACHINE_GUN';
+
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100', MustPurchase=0
 WHERE  UnitType = 'UNIT_LANDSKNECHT';
@@ -85,7 +97,6 @@ WHERE  UnitType = 'UNIT_MEDICINE_MAN';
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL,  Cost='1', BaseMoves='100'
 WHERE  UnitType = 'UNIT_JINETE';
-
 
 UPDATE Units
 SET    PrereqTech=NULL, StrategicResource=NULL, Cost='1', BaseMoves='100'
